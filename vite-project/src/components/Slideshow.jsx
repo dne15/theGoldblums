e052d19dceaa24152bea239e8ec540cbfb162be5
Maria <maria.g1611@yahoo.com>
2024-09-19 21:40:44 +0100

    I have added a slideshow that loops

--- vite-project/src/components/Slideshow.jsx
+++ vite-project/src/components/Slideshow.jsx
@@ -1,36 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Slideshow() {
   const [currentSlide, setCurrentSlide] = useState(0)
   const slides = [
-    '../images/JG1.webp',
-    '../images/JG2.webp',
-    '../images/JG3.jpg',
-    '../images/JG4.jpg',
-    '../images/JG5.jpg',
-    '../images/JG6.jpg',
-    '../images/JG7.jpg',
+    '/images/JG1.webp',
+    '/images/JG2.jpg',
+    '/images/JG3.jpg',
+    '/images/JG4.jpg',
+    '/images/JG5.png',
+    '/images/JG6.jpg',
+    '/images/JG7.jpg',
+    '/images/JG8.jpg',
+    '/images/JG9.jpg',
+    '/images/JG10.jpg',
+    '/images/JG11.jpg',
+    '/images/JG12.webp',
+    '/images/JG13.jpg',
+  
+ 
   ]
 
-  const nextSlide = () => {
-    setCurrentSlide((prev) => (prev + 1) % slides.length)
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length)
+    }, 3000) // Slide changes every 3 seconds
 
-  const prevSlide = () => {
-    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length)
-  }
+    return () => clearInterval(interval) // Cleanup on unmount
+  }, [slides.length])
 
   return (
-    <div className='slideshow'>
-      <h3>Our Golden Oldie Through the Years</h3>
-      <img src={slides[currentSlide]} alt={`Slide ${currentSlide + 1}`} className="slideshow-image" />
-      <div className='arrows'>
-        <button onClick={prevSlide}>
-          &#8249;
-        </button>
-        <button onClick={nextSlide}>
-          &#8250;
-        </button>
+    <div className='slideshow-wrapper'>
+      <h3 className="slideshow-title">Our Golden Oldie Through the Years</h3>
+      <div className="slideshow-container">
+        <img
+          src={slides[currentSlide]}
+          alt={`Slide ${currentSlide + 1}`}
+          className="slideshow-image"
+        />
       </div>
     </div>
   )
